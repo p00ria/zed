c246daeecb15ddd17145c264150666fcbdd35764
Lodingbot <alimohamad0409@gmail.com>
2016-04-07 14:33:07 +0000

    Create HELLO.lua

---
+++ plugins/HELLO.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "🙋🙌سلام " .. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "سلام کن به [name]",
+  patterns = {
+    "^سلام کن به (.*)$",
+    "^سلام کن به (.*)$"
+  }, 
+  run = run 
+}
+
+end
