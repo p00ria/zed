7e11683b4064f98ef9eaf22db3439ed60a7b5d5b
Lodingbot <alimohamad0409@gmail.com>
2016-04-10 11:26:30 +0000

    Update VERSION.lua

--- plugins/VERSION.lua
+++ plugins/VERSION.lua
@@ -1,9 +1,9 @@
 do
 
 function run(msg, matches)
-  return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
-  GNU GPL v2 license.]]
+  return 'Telegram 🤖 '.. VERSION .. [[ 
+  Checkout VERSION BOT IS:
+  2V ADMIN IS:@SHO_KER_ADMIN .]]
 end
 
 return {
@@ -11,6 +11,9 @@ return {
   usage = "!version: Shows bot version",
   patterns = {
     "^!version$"
+    "^ورژن$"
+    "^version$"
+    "^Version$"
   }, 
   run = run 
 }
