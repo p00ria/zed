5abbc15145dbd7812aaa2dccbceabdfb4b83385e
Lodingbot <alimohamad0409@gmail.com>
2016-04-10 11:12:35 +0000

    Update 2PHOTO.lua

--- plugins/2PHOTO.lua
+++ plugins/2PHOTO.lua
@@ -24,7 +24,7 @@ local function run(msg,matches)
     end
     if matches[1] == "tophoto" and is_momod(msg) then
     	redis:set("sticker:photo", "waiting")
-    	return 'Please send your sticker now'
+    	return 'لطفا حالا استیکر خود را بفرستید'
     end
 end
 return {
